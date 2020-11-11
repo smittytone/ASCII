5e73a491a69b054d1d81b628c3529343711e3cb3
Tony Smith <smittytone@icloud.com>
2020-11-11 14:55:00 +0000

    Add constants

---
+++ Source/Constants.h
@@ -0,0 +1,18 @@
+//
+//  Constants.h
+//  ASCII
+//
+//  Created by Tony Smith on 11/11/2020.
+//  Copyright © 2020 Tony Smith. All rights reserved.
+//
+
+#ifndef Constants_h
+#define Constants_h
+
+#define kColourWhite            0
+#define kColourRed              1
+#define kColourGreen            2
+#define kColourYellow           3
+#define kColourBlack            4
+
+#endif /* Constants_h */
